afb869b48b3c3bf1fa48816733ddfc174a97090d
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-06-28 19:50:24 +0545

    added all posts html

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.starting_page, name="starting_page"),
-    path('posts', views.posts, name="posts-page"),
-    path('posts/<slug:slug>', views.post_detail, name="post_detail")
+    path('posts/', views.posts, name="posts-page"),
+    path('posts/<slug:slug>/', views.post_detail, name="post_detail"),
 ]
