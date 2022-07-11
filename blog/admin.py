53dfe09adc8be617a411490256d33d42766fffa2
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-07-11 20:25:47 +0545

    added sessions, forms and view classes'

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from blog.models import Author, Post, Tag
+from blog.models import Author, Post, Tag, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -8,9 +8,13 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "date", "author",)
     list_filter = ("author", "tags", "date",)
 
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post",)
 # Register your models here.
 
 
 admin.site.register(Author)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -25,10 +25,21 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=254)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(
         Author, on_delete=models.SET_NULL, related_name='posts', null=True)
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField(max_length=254)
+    text = models.TextField(max_length=500)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
