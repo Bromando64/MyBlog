408d5ae3ac0ca5e27d8ccb2ac7d21bfbaf5d3686
Nishant Parajuli <nishant.parajuli123@gmail.com>
2022-07-11 17:14:10 +0545

    database and admin section created

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from blog.models import Author, Post, Tag
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "date", "author",)
+    list_filter = ("author", "tags", "date",)
+
 # Register your models here.
+
+
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
